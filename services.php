6e781a8580c74aff4c2bc5e92d341acb134474e0
ZakariaSaqi <sakizakaria7@gmail.com>
2023-06-04 17:51:38 +0000

    lol

--- services.php
+++ services.php
@@ -1,131 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>BETA ECO</title>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <link
-      rel="stylesheet"  href="bootstrap/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="fonts/css/all.min.css" />
-    <link rel="stylesheet" href="css/animate.css" />
-    <link rel="shortcut icon" href="images/iconWhite.svg" type="image/x-icon" />
-    <link rel="stylesheet" href="css/owl.carousel.min.css" />
-    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
-    <link rel="stylesheet" href="css/magnific-popup.css" />
-    <link rel="stylesheet" href="css/flaticon.css" />
-    <link rel="stylesheet" href="css/style.css" />
-  </head>
-  <body>
+
+<head>
+  <title>BETA ECO - Services</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <?php include('links.css') ?>
+</head>
+
+<body>
   <?php include('navbar.html') ?>
-    <!-- END nav -->
-    <section
-      class="hero-wrap hero-wrap-2"
-      style="background-image: url('images/bg_2.jpg')"
-      data-stellar-background-ratio="0.5"
-    >
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-end">
-          <div class="col-md-9 ftco-animate pb-5">
-            <p class="breadcrumbs mb-2">
-              <span class="mr-2"
-                ><a href="index.html"
-                  >Accueil <i class="ion-ios-arrow-forward"></i></a
-              ></span>
-              <span>Services <i class="ion-ios-arrow-forward"></i></span>
-            </p>
-            <h1 class="mb-0 bread">Nos services</h1>
-          </div>
+  <!-- END nav -->
+  <section class="hero-wrap hero-wrap-2" style="background-image: url('assets/images/bg_2.jpg')"
+    data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row no-gutters slider-text align-items-end">
+        <div class="col-md-9 ftco-animate pb-5">
+          <p class="breadcrumbs mb-2">
+            <span class="mr-2"><a href="index.html">Accueil <i class="ion-ios-arrow-forward"></i></a></span>
+            <span>Services <i class="ion-ios-arrow-forward"></i></span>
+          </p>
+          <h1 class="mb-0 bread">Nos services</h1>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section class="ftco-section">
-      <div class="container">
-        <div class="row">
-          <div
-            class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate"
-          >
-            <div class="d-block">
-              <div class="icon d-flex mr-2">
-                <span class="fa-solid fa-calculator"></span>
-              </div>
-              <div class="media-body">
-                <h3 class="heading">Comptabilité</h3>
-                <p>
-                  Notre équipe d'experts comptables compétents et expérimentés
-                  est prête à vous assister dans tous les aspects de votre
-                  comptabilité. Que vous soyez une petite entreprise, une
-                  société commerciale ou un particulier, nous offrons des
-                  services de tenue de livres, d'établissement des états
-                  financiers, de déclarations fiscales et de conseils
-                  personnalisés pour vous aider à optimiser votre situation
-                  financière.
-                </p>
-              </div>
+  <section class="ftco-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
+          <div class="d-block">
+            <div class="icon d-flex mr-2">
+              <span class="fa-solid fa-calculator"></span>
+            </div>
+            <div class="media-body">
+              <h3 class="heading">Comptabilité</h3>
+              <p>
+                Notre équipe d'experts comptables compétents et expérimentés
+                est prête à vous assister dans tous les aspects de votre
+                comptabilité. Que vous soyez une petite entreprise, une
+                société commerciale ou un particulier, nous offrons des
+                services de tenue de livres, d'établissement des états
+                financiers, de déclarations fiscales et de conseils
+                personnalisés pour vous aider à optimiser votre situation
+                financière.
+              </p>
             </div>
           </div>
-          <div
-            class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate"
-          >
-            <div class="d-block">
-              <div class="icon d-flex mr-2">
-                <span class="fa-solid fa-graduation-cap"></span>
-              </div>
-              <div class="media-body">
-                <h3 class="heading">Formation</h3>
-                <p>
-                  Nous croyons en l'importance de l'apprentissage
-                  continu. C'est pourquoi nous proposons des programmes de
-                  formation professionnelle dans le domaine de la comptabilité,
-                  adaptés à différents niveaux d'expérience. Que vous soyez
-                  débutant ou professionnel chevronné, nos cours vous
-                  permettront d'acquérir de nouvelles compétences, de rester à
-                  jour avec les dernières normes comptables et de développer
-                  votre expertise.
-                </p>
-              </div>
+        </div>
+        <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
+          <div class="d-block">
+            <div class="icon d-flex mr-2">
+              <span class="fa-solid fa-graduation-cap"></span>
+            </div>
+            <div class="media-body">
+              <h3 class="heading">Formation</h3>
+              <p>
+                Nous croyons en l'importance de l'apprentissage
+                continu. C'est pourquoi nous proposons des programmes de
+                formation professionnelle dans le domaine de la comptabilité,
+                adaptés à différents niveaux d'expérience. Que vous soyez
+                débutant ou professionnel chevronné, nos cours vous
+                permettront d'acquérir de nouvelles compétences, de rester à
+                jour avec les dernières normes comptables et de développer
+                votre expertise.
+              </p>
             </div>
           </div>
-          <div
-            class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate"
-          >
-            <div class="d-block">
-              <div class="icon d-flex mr-2">
-                <span class="fa-solid fa-gavel"></span>
-              </div>
-              <div class="media-body">
-                <h3 class="heading">Conseil Juridique</h3>
-                <p>
-					Nos consultants juridiques qualifiés sont là pour vous fournir des conseils juridiques fiables et adaptés à vos besoins. Que vous ayez besoin d'aide pour la création d'entreprise, la rédaction de contrats, la résolution de litiges ou toute autre question juridique, nous mettons notre expertise à votre disposition pour vous guider à chaque étape.
-                </p>
-              </div>
+        </div>
+        <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
+          <div class="d-block">
+            <div class="icon d-flex mr-2">
+              <span class="fa-solid fa-gavel"></span>
+            </div>
+            <div class="media-body">
+              <h3 class="heading">Conseil Juridique</h3>
+              <p>
+                Nos consultants juridiques qualifiés sont là pour vous fournir des conseils juridiques fiables et
+                adaptés à vos besoins. Que vous ayez besoin d'aide pour la création d'entreprise, la rédaction de
+                contrats, la résolution de litiges ou toute autre question juridique, nous mettons notre expertise à
+                votre disposition pour vous guider à chaque étape.
+              </p>
             </div>
           </div>
-          <div
-            class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate"
-          >
-            <div class="d-block">
-              <div class="icon d-flex mr-2">
-                <span class="fa-solid fa-wheat-awn"></span>
-              </div>
-              <div class="media-body">
-                <h3 class="heading">Gestion d'Exploitation Agricole</h3>
-                <p>
-					Nous comprenons les défis auxquels sont confrontées les exploitations agricoles. Notre équipe spécialisée en gestion d'exploitation agricole offre des services de conseil et d'accompagnement pour optimiser les performances de votre exploitation. De la planification financière à la gestion des ressources, nous vous aidons à prendre des décisions éclairées pour assurer la croissance et la rentabilité de votre entreprise agricole.
-                </p>
-              </div>
+        </div>
+        <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
+          <div class="d-block">
+            <div class="icon d-flex mr-2">
+              <span class="fa-solid fa-wheat-awn"></span>
+            </div>
+            <div class="media-body">
+              <h3 class="heading">Gestion d'Exploitation Agricole</h3>
+              <p>
+                Nous comprenons les défis auxquels sont confrontées les exploitations agricoles. Notre équipe
+                spécialisée en gestion d'exploitation agricole offre des services de conseil et d'accompagnement pour
+                optimiser les performances de votre exploitation. De la planification financière à la gestion des
+                ressources, nous vous aidons à prendre des décisions éclairées pour assurer la croissance et la
+                rentabilité de votre entreprise agricole.
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
 
-    <!-- <section class="ftco-section bg-light ftco-no-pt">
+  <!-- <section class="ftco-section bg-light ftco-no-pt">
       <div class="container">
         <div class="row justify-content-center pb-5 mb-3">
           <div class="col-md-7 heading-section text-center ftco-animate">
@@ -262,45 +245,18 @@
       </div>
     </section> -->
 
-    <?php include('footer.php') ?>
-    <!-- loader -->
-    <div id="ftco-loader" class="show fullscreen">
-      <svg class="circular" width="48px" height="48px">
-        <circle
-          class="path-bg"
-          cx="24"
-          cy="24"
-          r="22"
-          fill="none"
-          stroke-width="4"
-          stroke="#eeeeee"
-        />
-        <circle
-          class="path"
-          cx="24"
-          cy="24"
-          r="22"
-          fill="none"
-          stroke-width="4"
-          stroke-miterlimit="10"
-          stroke="#F96D00"
-        />
-      </svg>
-    </div>
+  <?php include('footer.php') ?>
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen">
+    <svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+        stroke="#F96D00" />
+    </svg>
+  </div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/jquery.animateNumber.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/scrollax.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-    <script src="js/google-map.js"></script>
-    <script src="js/main.js"></script>
-  </body>
+  <?php include('links.js') ?>
+
+</body>
+
 </html>
