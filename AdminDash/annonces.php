9d0abc774a1a8869b7ab32a7877a0a2024280864
ZakariaSaqi <sakizakaria7@gmail.com>
2023-06-06 06:08:32 +0000

    l

---
+++ AdminDash/annonces.php
@@ -0,0 +1,139 @@
+<html lang="en">
+
+<head>
+    <title>Admin - Annonces</title>
+    <?php include('links.html') ?>
+</head>
+<body>
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <?php include('navbar.php') ?>
+        <div class="container-fluid">
+            <div class="container">
+
+                <div class="row mb-2 d-flex align-items-center ">
+                    <div class="col-md-6 nav-small-cap">
+                        <h4>Gestion des annonces</h4>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="search">
+                            <input type="text" class="form-control ps-0"
+                                placeholder="ID, Titres, Description, Date ...">
+                            <button class="btn btn-primary"><i class="fa fa-search" style="color:white"></i></button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col" colspan="3">
+                        <button class="btn btn-light py-2 px-4">
+                            <i class="fa-solid fa-square-plus me-3" style="font-size: 1.3rem;"></i>
+                            <p class="text-uppercase m-0">Ajouter une nouvelle annonce</p>
+                        </button>
+                        </th>
+                        <th scope="col" colspan="2">
+                        <button class="btn btn-light py-2 px-4">
+                            <i class="fa-solid fa-square-plus me-3" style="font-size: 1.3rem;"></i>
+                            <p class="text-uppercase m-0">Exporter au format PDF</p>
+                        </button>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Titre</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Date</th>
+                        <th scope="col" style="width:50px">Opération</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Zakaria SAKI</td>
+                        <td>sakizakaria7@gmail.com</td>
+                        <td>Formation</td>
+                        <td class="row">
+                            <a href="operations_demande.php?action=view&id=<?= $data['id'] ?>" class="view col-4"
+                                title="View" data-toggle="tooltip"><i class="fa-solid fa-circle-info"></i></a>
+                            <a href="operations_demande.php?action=Edit&id=<?= $data['id'] ?>" class="edit col-4"
+                                title="Edit" data-toggle="tooltip"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="operations_demande.php?action=Delete&id=<?= $data['id'] ?>" class="delete col-4"
+                                title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Zakaria SAKI</td>
+                        <td>sakizakaria7@gmail.com</td>
+                        <td>Formation</td>
+                        <td class="row">
+                            <a href="operations_demande.php?action=view&id=<?= $data['id'] ?>" class="view col-4"
+                                title="View" data-toggle="tooltip"><i class="fa-solid fa-circle-info"></i></a>
+                            <a href="operations_demande.php?action=Edit&id=<?= $data['id'] ?>" class="edit col-4"
+                                title="Edit" data-toggle="tooltip"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="operations_demande.php?action=Delete&id=<?= $data['id'] ?>" class="delete col-4"
+                                title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Zakaria SAKI</td>
+                        <td>sakizakaria7@gmail.com</td>
+                        <td>Formation</td>
+                        <td class="row">
+                            <a href="operations_demande.php?action=view&id=<?= $data['id'] ?>" class="view col-4"
+                                title="View" data-toggle="tooltip"><i class="fa-solid fa-circle-info"></i></a>
+                            <a href="operations_demande.php?action=Edit&id=<?= $data['id'] ?>" class="edit col-4"
+                                title="Edit" data-toggle="tooltip"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="operations_demande.php?action=Delete&id=<?= $data['id'] ?>" class="delete col-4"
+                                title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Zakaria SAKI</td>
+                        <td>sakizakaria7@gmail.com</td>
+                        <td>Formation</td>
+                        <td class="row">
+                            <a href="operations_demande.php?action=view&id=<?= $data['id'] ?>" class="view col-4"
+                                title="View" data-toggle="tooltip"><i class="fa-solid fa-circle-info"></i></a>
+                            <a href="operations_demande.php?action=Edit&id=<?= $data['id'] ?>" class="edit col-4"
+                                title="Edit" data-toggle="tooltip"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="operations_demande.php?action=Delete&id=<?= $data['id'] ?>" class="delete col-4"
+                                title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Zakaria SAKI</td>
+                        <td>sakizakaria7@gmail.com</td>
+                        <td>Formation</td>
+                        <td class="row">
+                            <a href="operations_demande.php?action=view&id=<?= $data['id'] ?>" class="view col-4"
+                                title="View" data-toggle="tooltip"><i class="fa-solid fa-circle-info"></i></a>
+                            <a href="operations_demande.php?action=Edit&id=<?= $data['id'] ?>" class="edit col-4"
+                                title="Edit" data-toggle="tooltip"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="operations_demande.php?action=Delete&id=<?= $data['id'] ?>" class="delete col-4"
+                                title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
--- AdminDash/index.php
+++ AdminDash/index.php
@@ -1,156 +1,14 @@
 <!doctype html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Modernize Free</title>
-  <link rel="shortcut icon" href="../assets/images/iconWhite.svg" type="image/x-icon">
-  <link rel="stylesheet" href="assets/css/styles.min.css" />
-  <link rel="stylesheet" href="../assets/icons/css/all.css">
+  <title>Admin - Accueil </title>
+  <?php include('links.html') ?>
 </head>
-
 <body>
   <!--  Body Wrapper -->
   <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
     data-sidebar-position="fixed" data-header-position="fixed">
-    <!-- Sidebar Start -->
-    <aside class="left-sidebar ">
-      <!-- Sidebar scroll-->
-      <div>
-        <div class="brand-logo d-flex align-items-center justify-content-between">
-          <a href="../index.php" class="text-nowrap logo-img">
-            <img src="../assets/images/iconBlack.svg" width="40" alt="" />
-          </a>
-          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-            <i class="ti ti-x fs-8"></i>
-          </div>
-        </div>
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-          <ul id="sidebarnav">
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Cabinet comptable</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="#" aria-expanded="false">
-                <span>
-                  <i class="fa-solid fa-users"></i>
-                </span>
-                <span class="hide-menu">Clients</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="#" aria-expanded="false">
-                <span>
-                  <i class="fa-sharp fa-solid fa-bullhorn"></i>
-                </span>
-                <span class="hide-menu">Annonces</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="#" aria-expanded="false">
-                <span>
-                  <i class="fa-sharp fa-solid fa-list"></i>
-                </span>
-                <span class="hide-menu">Services</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="#" aria-expanded="false">
-                <span>
-                  <i class="fa-solid fa-comment"></i>
-                </span>
-                <span class="hide-menu">Commentaires</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Cours soutien</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="#" aria-expanded="false">
-                <span>
-                  <i class="fa-solid fa-users"></i>
-                </span>
-                <span class="hide-menu">Etudiants</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="#" aria-expanded="false">
-                <span>
-                  <i class="fa-solid fa-book"></i>
-                </span>
-                <span class="hide-menu">Cours</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="#" aria-expanded="false">
-                <span>
-                  <i class="fa-solid fa-book"></i>
-                </span>
-                <span class="hide-menu">Exercices</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <!-- End Sidebar navigation -->
-      </div>
-      <!-- End Sidebar scroll-->
-    </aside>
-    <!--  Sidebar End -->
-    <!--  Main wrapper -->
-    <div class="body-wrapper">
-      <!--  Header Start -->
-      <header class="app-header">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <ul class="navbar-nav">
-            <li class="nav-item d-block d-xl-none">
-              <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
-                <i class="ti ti-menu-2"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link nav-icon-hover" href="javascript:void(0)">
-                <i class="fa-solid fa-house"></i>
-                 </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link nav-icon-hover" href="javascript:void(0)">
-                <i class="fa-solid fa-bell"></i>
-                <div class="notification bg-primary rounded-circle"></div>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link nav-icon-hover" href="javascript:void(0)">
-                <i class="fa-solid fa-message"></i>
-                <div class="notification bg-primary rounded-circle"></div>
-              </a>
-            </li>
-          </ul>
-          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
-            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-              <li class="nav-item dropdown">
-                <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  <i class="fa-solid fa-user"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
-                  <div class="message-body">
-                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                      <i class="ti ti-mail fs-6"></i>
-                      <p class="mb-0 fs-3">My Account</p>
-                    </a>
-                    <a href="./authentication-login.html" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <!--  Header End -->
+    <?php include('navbar.php') ?>
       <div class="container-fluid">
         <!--  Row 1 -->
         <div class="row">
@@ -331,13 +189,6 @@
       </div>
     </div>
   </div>
-  <script src="assets/libs/jquery/dist/jquery.min.js"></script>
-  <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/js/sidebarmenu.js"></script>
-  <script src="assets/js/app.min.js"></script>
-  <script src="assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-  <script src="assets/libs/simplebar/dist/simplebar.js"></script>
-  <script src="assets/js/dashboard.js"></script>
 </body>
 
 </html>
